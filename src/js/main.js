5cc6cb3baa515721379a576345eab8db9b30f65e
hanaraya <n.harish22@gmail.com>
2016-03-23 23:30:20 -0700

    offline caching html

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,7 @@
 var stationList = document.getElementById('station-list');
-
-var stationTestArray = [{name: "Fremont", abbr: "FRMT"}, {name: "Oakland" , abbr: "12TH"}];
-
 var optionHTML = '<option value="%value-data%" abbreviation="%abbr-data%">%abbr-data%</option>';
-
 var stationInnerHTML = '';
-
 var stationRequest = new Request('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V');
-//var stationRequest = new Request('http://api.bart.gov/api/bsa.aspx?cmd=count&key=MW9S-E7SL-26DU-VV8V');
-// var stations = fetch(stationRequest).then(function(response){
-// 	if(!response) return;
-// 	var contentType = response.headers.get('content-type');
-// 	console.log('content type ', contentType);
-
-// 	response.text().then(function(textxml){
-// 		var parser = new DOMParser();
-// 		console.log(textxml);
-// 	var xmlDoc = parser.parseFromString(textxml,"text/xml");
-// 	console.log(xmlDoc);
-// 	console.log(xmlDoc.getElementsByTagName('station'));
-	// .forEach(function(station){
-	// 	console.log(station);
-	// });
-	// console.log(xmlDoc.getElementsByTagName('traincount')[0].innerHTML);
-	// console.log(xmlDoc.getElementsByTagName('root').nodeValue);
-	// });
-
-	//console.log(response.arrayBuffer());
-	// response.text().then(function(reader){
-	// 	//console.log(reader.find('traincount'));
-	// 	//var x = new XML(reader);
-	// 	console.log(XML.parse(reader));
-	// });
-// });
 
 function getXML(request){
 	return fetch(request).then(function(response){
@@ -51,10 +20,6 @@ function getJSON(request){
 
 function populateStations(){
 	return getJSON(stationRequest).then(function(responseJSON){ 
-		console.log(responseJSON);
-		console.log(responseJSON.root.stations);		
-		console.log(responseJSON.root.stations.station[0].name['#text']);
-		console.log(JSON.stringify(responseJSON));
 		responseJSON.root.stations.station.forEach(function(station){
 			stationInnerHTML+=optionHTML.replace('%value-data%', station.name['#text']).replaceAll('%abbr-data%',station.abbr['#text']);
 		});
@@ -66,12 +31,15 @@ String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
-
-// stationTestArray.forEach(function(station){
-// 	stationInnerHTML += optionHTML.replace('%value-data%', station.name).replace('%abbr-data%', station.abbr);
-// });
 populateStations();
-console.log(stationInnerHTML);
+
+//Register service worker
+if(navigator.serviceWorker){
+	navigator.serviceWorker.register('sw.js', {scope:'/'}).then(function(registration){ 
+	}).catch(function(error){ 
+		console.log(error);
+	});
+}
 
 
 // Changes XML to JSON
